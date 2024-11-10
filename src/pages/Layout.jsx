4e292ab61879f6f0b23151014199c469ef15ae3a
Walter Lazo <walterlazo293@gmail.com>
2024-11-09 20:06:55 -0600

    Update Files

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from './Home';
+import Header from './Navbar';
 import { Helmet } from 'react-helmet';
 const Layout = ({ title, children }) => {
   return (
