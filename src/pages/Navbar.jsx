5827100696573b5c922831677db2128424d5ddc7
Kevin Jiménez <kvn.jimenez.t@gmail.com>
2024-11-18 17:56:24 -0600

    Update: Bold Font

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -4,68 +4,75 @@ import LoginAuth from '../components/LoginAuth';
 import { auth } from '../config/firebase';
 import { UserContext } from '../context/UserContext';
 import { useNavigate } from 'react-router-dom';
+
 const Header = () => {
   const { user, userPhoto, setUserPhoto } = useContext(UserContext);
   const [showLogin, setShowLogin] = useState(false);
   const [scrolled, setScrolled] = useState(false);
   const navigate = useNavigate();
-    const handleLogin = (loggedInUser) => {
-      if (loggedInUser) {
-        const user = auth.currentUser;
-        setUserPhoto(user.photoURL);
-      }
-    };
-    const handleUserPhotoClick = () => {
-      if (user) {
-        navigate('/profile');
-      } else {
-        setShowLogin(!showLogin);
-      }
-    };
-    useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY > 50) {
-          setScrolled(true);
-        } else {
-          setScrolled(false);
-        }
-      };
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-    return (
-      <HeaderContainer className={scrolled ? 'scrolled' : ''}>
-        <Nav>
-          <LogoContainer className="logo-container">
-            <LogoLink href="/">
-              <img src="src/assets/Logo Costa Rica Hills sin fondo.png" alt="Logo" />
-              <span>Costa Rica Hills</span>
-            </LogoLink>
-          </LogoContainer>
-          <NavLinks className="nav-links">
-            <a href="/tours">Tours</a>
-            <a href="/about">Sobre Nosotros</a>
-            <UserPhoto
-              src={userPhoto}
-              alt="Foto de Usuario"
-              id="user-photo"
-              onClick={handleUserPhotoClick} 
-            />
-          </NavLinks>
-        </Nav>
-        {showLogin && !user && <LoginAuth onLogin={handleLogin} />} {/* Renderiza LoginAuth si showLogin es true y no hay usuario */}
-      </HeaderContainer>
-    );
+
+  const handleLogin = (loggedInUser) => {
+    if (loggedInUser) {
+      const user = auth.currentUser;
+      setUserPhoto(user.photoURL);
+    }
   };
+
+  const handleUserPhotoClick = () => {
+    if (user) {
+      navigate('/profile');
+    } else {
+      setShowLogin(!showLogin);
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <HeaderContainer className={scrolled ? 'scrolled' : ''}>
+      <Nav>
+        <LogoContainer className="logo-container">
+          <LogoLink href="/">
+            <img src="src/assets/Logo Costa Rica Hills sin fondo.png" alt="Logo" />
+            <span>Costa Rica Hills</span>
+          </LogoLink>
+        </LogoContainer>
+        <NavLinks className="nav-links">
+          <a href="/tours">Tours</a>
+          <a href="/about">Sobre Nosotros</a>
+          <UserPhoto
+            src={userPhoto}
+            alt="Foto de Usuario"
+            id="user-photo"
+            onClick={handleUserPhotoClick} 
+          />
+        </NavLinks>
+      </Nav>
+      {showLogin && !user && <LoginAuth onLogin={handleLogin} />} {/* Renderiza LoginAuth si showLogin es true y no hay usuario */}
+    </HeaderContainer>
+  );
+};
+
 export default Header;
+
 const HeaderContainer = styled.header`
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  background-color: transparent;
+  background-color: rgba(0, 0, 0, 0.5); /* Fondo con opacidad casi negra */
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   padding: 1rem;
   transition: background-color 0.3s ease;
@@ -78,6 +85,7 @@ const HeaderContainer = styled.header`
     color: black; /* Cambia el color del texto a negro */
   }
 `;
+
 const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
@@ -86,15 +94,18 @@ const Nav = styled.nav`
   padding: 0 1.5rem; /* Añadir padding para que no se pegue a los bordes */
   margin: 0 auto;
 `;
+
 const LogoLink = styled.a`
   display: flex;
   align-items: center;
   text-decoration: none;
   color: inherit;
+  font-weight: bold; /* Texto en negrita */
   span:hover {
     color: #00a08b; /* Gris oscuro al hacer hover */
   }
 `;
+
 const LogoContainer = styled.div`
   display: flex;
   align-items: center;
@@ -109,6 +120,7 @@ const LogoContainer = styled.div`
     color: #ffffff; /* Gris oscuro */
   }
 `;
+
 const NavLinks = styled.div`
   display: flex;
   justify-content: center;
@@ -118,11 +130,13 @@ const NavLinks = styled.div`
     color: #ffffff; /* Gris intermedio */
     text-decoration: none;
     transition: color 0.3s ease;
+    font-weight: bold; /* Texto en negrita */
     &:hover {
       color: #00a08b; /* Gris oscuro al hacer hover */
     }
   }
 `;
+
 const UserPhoto = styled.img`
   position: relative;
   height: 2.5rem;
